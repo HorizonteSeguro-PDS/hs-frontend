e82ade6272ed849343c10d4c757947490f36a241
João Raphael <jrlgo@ic.ufal.br>
2026-06-07 01:17:20 -0300

    adição de padding, e colocando navbar na tela home.

--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -3,7 +3,7 @@ import { ChevronDown, CircleUserRound } from 'lucide-preact';
 //TODO CRIAR UM CONTEXTO PARA A IMAGEM DO USUÁRIO E O NOME DO USUÁRIO, PARA EXIBIR NA NAVBAR
 export const Navbar = () => {
     return (
-        <div className="navbar flex h-20 w-full items-center justify-between">
+        <div className="navbar flex h-20 w-full items-center justify-between px-[10%]" style={{ backgroundColor:'#111111'}}>
             <img src={NavbarLogo} alt="Logo horizonte seguro" className=""/>
             <div className="flex items-center gap-4">
                 <CircleUserRound size={40} />
